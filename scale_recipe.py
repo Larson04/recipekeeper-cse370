75d96f65e4f5a40f495a3d581adba743ae0e3402
Fernando Ferraez-Rincon <167939809+cachito2002@users.noreply.github.com>
2025-09-29 14:00:48 -0600

    added a sample file to test

---
+++ scale_recipe.py
@@ -0,0 +1,10 @@
+from fractions import Fraction
+import math
+
+cups_of_flower = 1 
+
+targets_cups_f = 3
+
+cups_needed = Fraction(targets_cups_f/cups_of_flower)
+
+print(f"Cups of flower: {cups_needed}")
