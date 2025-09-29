71395a1290c2b6c4bd5d1156b9d6aa6ba058213b
EthanE677 <eva21020@byui.edu>
2025-09-29 11:06:25 -0600

    parsing

--- recipe.py
+++ recipe.py
@@ -3,6 +3,24 @@ import json
 #hello there
 def recipe_multi(PATH, Multiplier):
     #open le json
-    with open('PATH', 'r') as file:
+    with open(PATH, 'r') as file:
         data = json.load(file)
     #use le json
+    ingredients = []
+    i = 0
+    for name, details in data["ingredients"].items():
+        ingredients.append([name])
+        if details['amount']:
+            ingredients[i].append(details['amount'])
+        if details['unit']:  # only add if it's not an empty string
+            ingredients[i].append(details['unit'])
+        if details['note']:  # only add if it's not an empty string
+            ingredients[i].append(details['note'])
+        i += 1
+
+        #print(f"{name}: {details['amount']} {details['unit']}, {details['note']}")
+    print(ingredients)
+
+
+
+recipe_multi("alfredo-recipe.json", 1)
