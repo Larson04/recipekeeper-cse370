a7ad937d7afe6245fc93aff3b10f3b03a1c1c06d
Larson04 <lar23016@byui.edu>
2025-10-22 13:56:40 -0600

    update tests

--- tests.py
+++ tests.py
@@ -4,34 +4,47 @@ print file to terminal for testing
 multiply measurements correctly
 convert measurement units correctly
 '''
-from recipe import recipe_multi
+from recipe import get_ingredients, standardize_units
 
 def test_recipe_parsing_1():
     '''
-    Test the recipe_multi function with a simple list of ingredients.
+    Test the get_ingredients function with a simple list of ingredients.
     The recipe is a JSON string with three ingredients: an egg, 2 tablespoons of flour, and 1 stick of butter.
-    The expected output is a list of three lists, each containing the name, amount, unit, and note of an ingredient.
+    The expected output is a list of three lists, each containing the name, amount, and if they have them, unit, and 
+    note of an ingredient.
     '''
-    recipe = {"ingredients": {
-    "egg": {"amount": 1, "unit": " ", "note": " "}, 
-    "flour": {"amount": 2, "unit": "tablespoons", "note": " "}, 
-    "butter": {"amount": 1, "unit": "stick", "note": "softened"}
-    }
-    }
-    expected = [['egg', 1, ' ', ' '], ['flour', 2, 'tablespoons', ' '], ['butter', 1, 'stick', 'softened']]
-    assert recipe_multi(recipe) == expected
+    recipe = "test.json"
+    expected = [
+        ['egg', 1,], 
+        ['flour', 2, 'tablespoons'], 
+        ['butter', 1, 'stick', 'softened']]
+    assert get_ingredients(recipe) == expected
     print("test_recipe_multi_1 passed")
 
 def test_recipe_parsing_2():
-    '''Test '''
-    recipe = "test.json"
-    expected = [['salted-butter', 12, 'tbsp', ''], 
-                ['garlic', 2, 'tbsp', 'minced'], 
-                ['flour', 4, 'tbsp', ''], 
-                ['heavy-cream', 3, 'c', '']]
-    assert recipe_multi(recipe, 2) == expected
+    '''Test the get_ingredients function with a simple list of ingredients.
+    The recipe is a JSON string with three ingredients: salted butter, 1 tablespoon of garlic, 2 tablespoons of flour, 
+    and 1.5 cups of heavy cream.
+    The expected output is a list of three lists, each containing the name, amount, and if they have them, unit, and 
+    note of an ingredient.
+    '''
+    recipe = "test2.json"
+    expected = [
+        ['salted-butter', 6, 'tbsp'], 
+        ['garlic', 1, 'tbsp', 'minced'], 
+        ['flour', 2, 'tbsp'], 
+        ['heavy-cream', 1.5, 'c']]
+    assert get_ingredients(recipe) == expected
     print("test_recipe_multi_2 passed")
 
+def test_standardize_units_1():
+    '''
+    Test the standardize_units function with a simple list of ingredients with a mix of abbreviations and full words.
+    The recipe is a JSON string with three ingredients: salted butter, 1 tablespoon of garlic, 2 tablespoons of flour, 
+    and 1.5 cups of heavy cream.
+    The expected output is a list of three lists, each containing the name, amount, and if they have them, unit, and 
+    note of an ingredient.
+    '''
 
 def run_tests():
     test_recipe_parsing_1()
